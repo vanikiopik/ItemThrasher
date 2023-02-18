f79cd610329993b2364e7d8de8656af73ce895bf
vanikiopik <vanikiopik@gmail.com>
2023-02-18 17:00:27 +0300

    NEW: add sprites, simple mechanic of deleting objects

---
+++ Assets/Scripts/FindingObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class FindingObject : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("asd");
+        GameObject.Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+    }
+}
